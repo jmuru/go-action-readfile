a591f1c0cf2bcd7c49170309b6209e15004c0e57
jmuru <joseph.muruguru@nytimes.com>
2023-07-19 16:16:08 -0400

    build

--- save_content.js
+++ save_content.js
@@ -8,15 +8,39 @@ const database="blog-content";
 const uriA = `mongodb+srv://${username}:${password}@blog-content.rqepisu.mongodb.net/blog-content?retryWrites=true&w=majority`;
 const uriB = `mongodb://${username}:${password}@blog-content.rqepisu.mongodb.net/blog-content?retryWrites=true&w=majority`;
 
+// Define a Mongoose schema for the collection
+const recordSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
 async function connectToMongoDB(uri) {
   console.log(uri);
   try {
     await mongoose.connect(uri);
+
     console.log('Connected to MongoDB successfully!');
+    insertRecord('John Doe', 30);
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
   }
 }
+
+async function insertRecord(name, age) {
+  try {
+    // Create a new record instance
+    const newRecord = new Record({
+      name: name,
+      age: age,
+    });
+    // Save the record to the database
+    await newRecord.save();
+    console.log('Record inserted successfully:', newRecord);
+  } catch (error) {
+    console.error('Failed to insert record:', error);
+  }
+}
+
 // Usage
 const mongoURI = `mongodb://${username}:${password}@${host}:27017/${database}`;
 connectToMongoDB(uriA);
